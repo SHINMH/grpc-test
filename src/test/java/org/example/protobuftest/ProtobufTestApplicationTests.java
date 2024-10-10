6658315c1280854ce6800a74d4d2b7691cee2cd0
SHINMH <shin_mh@lgcns.com>
2024-10-11 01:27:55 +0900

    first init

---
+++ src/test/java/org/example/protobuftest/ProtobufTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.protobuftest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtobufTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
